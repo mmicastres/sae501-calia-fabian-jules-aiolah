e5147e2659fc9dd9a2f253e984f8a9b75e090550
aiolah <yoyo31.music@gmail.com>
2023-12-11 22:12:06 +0100

    Update

--- app/src/main/java/com/example/castresautresor/MainActivity.kt
+++ app/src/main/java/com/example/castresautresor/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
